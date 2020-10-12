cd2f6270e690161c4955b7cab8abd82be0850e6a
Faduma Abdillahi <faduma92@gmail.com>
2020-10-12 17:40:45 -0400

    deploying

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/budget', 
-    { useNewUrlParser: true }
-);
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );
+    
 
 const app = express();
 
